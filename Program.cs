4c8481ca47b26bfbf9ecfe7c526cb8ae2ef2d9db
danielalegria22 <danielalegria596@gmail>
2024-11-10 23:42:37 -0500

    segungo commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string seguir = "";
+int posicion = 0;
+string[]nombres=new string[0];   
+
+    do
+    {
+        Console.WriteLine("escriba el nombre del desayuno");
+        string nom = Console.ReadLine();
+        Array.Resize(ref nombres,nombres.Length+1);
+        nombres[posicion] = nom;
+        posicion++;
+        Console.WriteLine($"desayuno {nom} agregado");
+        Console.WriteLine("desea seguir registrando?[si]");
+         seguir =Console.ReadLine();
+    } while (seguir == "si");
+
+
+
