19e2eff1d8b35a005b82616753749a971a225ce7
WhiteK1ng <edsonvillanueva2018@gmail.com>
2024-11-10 23:24:30 -1200

    primer commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+int posicion = 0;
+Console.WriteLine("Ingresar nombre para eliminar: ");
+string eliminar = Console.ReadLine();
+int indice = Array.IndexOf(nombres,eliminar);
+if (indice != -1)
+{
+    for (int i = indice; i < nombres.Lenght - 1 )
+    {
+        nombres[i] = nombres[i + 1];
+    }
+    Array.Resize(ref nombres, nombres.Lenght - 1);
+    posicion--;
+    Console.WriteLine($"Desayuno {eliminar} eliminado");
+
+}else
+{
+    Console.WriteLine("No se puede eliminar porque no existe");
+}
+
+
