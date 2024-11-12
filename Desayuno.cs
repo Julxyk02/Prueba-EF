259ba111115438e109f4c0793968ca6e4b628cff
Julio Quiñonez <Julxyk02>
2024-11-12 13:37:31 -0500

    Menú desayuno

---
+++ Desayuno.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purbea
+{
+    internal class Desayuno{
+        public string[] nombres = new string[0];
+        int posicion = 0;
+        string seguir = "";
+        public void desayunos(){
+            do{
+                Console.WriteLine("escriba el nombre del desayuno");
+                string nom = Console.ReadLine();
+                Array.Resize(ref nombres, nombres.Length + 1);
+                nombres[posicion] = nom;
+                posicion++;
+                Console.WriteLine($"desayuno {nom} agregado");
+                Console.WriteLine("desea seguir registrando?[si/no]:");
+                seguir = Console.ReadLine();
+            } while (seguir == "si");
+        }
+
+        public void Lista(){
+            Console.WriteLine("Lista de desayunos:");
+            for (int i = 0; i < nombres.Length; i++){
+                Console.WriteLine($"- {nombres[i]} ");
+            }
+        }
+
+        public void Eliminar(){
+            Console.WriteLine("Ingresar nombre para eliminar:");
+            string eliminar = Console.ReadLine();
+            int indice = Array.IndexOf(nombres, eliminar);
+            if (indice != -1){
+                for (int i = indice; i < nombres.Length - 1; i++){
+                    nombres[i] = nombres[i + 1];
+
+                }
+                Array.Resize(ref nombres, nombres.Length - 1);
+                posicion--;
+                Console.WriteLine($"Desayuno {eliminar} eliminado");
+            }
+            else{
+                Console.WriteLine("No se puede eliminar porque no existe");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Purbea;
+
+Desayuno E = new Desayuno();
+string regresar = "";
+do{
+    Console.WriteLine("--- Menú de Desayunos ---");
+    Console.WriteLine("a) Crear desayuno");
+    Console.WriteLine("b) Listar desayunos");
+    Console.WriteLine("c) Eliminar desayuno");
+    Console.WriteLine("d) Fin");
+    Console.Write("Selecciona una opción: ");
+    string opcion = Console.ReadLine();
+
+    switch (opcion){
+        case "a":
+            E.desayunos();
+            break;
+        case "b":
+            E.Lista();
+            break;
+        case "c":
+            E.Eliminar();
+            break;
+        case "d":
+            Console.WriteLine("Esta seguro?");
+            break;
+        default:
+            Console.WriteLine("opcion no valida, vuelva a intentar");
+            break;
+    }
+
+    Console.WriteLine("regresar al menú [si/no]:");
+    regresar = Console.ReadLine();
+    Console.Clear();
+
+} while (regresar == "si");
